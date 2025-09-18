f5bc10a657ed7c0d26d8ba9f9d7b6c9f40c341a4
FVS <feliven@gmail.com>
2025-09-18 13:06:25 -0300

    adiciona itens sem recarregar página

--- script-crud.js
+++ script-crud.js
@@ -1,18 +1,13 @@
 const botaoAdicionarTarefa = document.querySelector(".app__button--add-task");
-
-// formulário e botões para criar nova tarefa
 const formularioAdicionarTarefa = document.querySelector(".app__form-add-task");
 const campoTextoAdicionarTarefa = document.querySelector(".app__form-textarea");
-// const botaoSalvarNovaTarefa = document.querySelector(".app__form-footer__button--confirm");
-// const botaoCancelarNovaTarefa = document.querySelector(".app__form-footer__button--cancel");
-// const botaoDeletarNovaTarefa = document.querySelector(".app__form-footer__button--delete");
-
 const elementoListaTarefas = document.querySelector(".app__section-task-list");
 
 const listaTarefas = JSON.parse(localStorage.getItem("listaTarefas")) || [];
 
-function criarElementoTarefa(tarefa) {
-  const elementoTarefa = `
+function criarElementoTarefa() {
+  listaTarefas.forEach((tarefa) => {
+    const elementoTarefa = `
     <li class="app__section-task-list-item">
       <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
@@ -27,8 +22,11 @@ function criarElementoTarefa(tarefa) {
     </li>
     `;
 
-  return elementoTarefa;
+    elementoListaTarefas.innerHTML += elementoTarefa;
+  });
 }
+
+criarElementoTarefa();
 
 botaoAdicionarTarefa.addEventListener("click", () => {
   formularioAdicionarTarefa.classList.remove("hidden");
@@ -39,11 +37,8 @@ formularioAdicionarTarefa.addEventListener("submit", (evento) => {
   const tarefa = { descricaoTarefa: campoTextoAdicionarTarefa.value };
   listaTarefas.push(tarefa);
   localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+  elementoListaTarefas.innerHTML = "";
+  criarElementoTarefa();
   campoTextoAdicionarTarefa.value = "";
   formularioAdicionarTarefa.classList.add("hidden");
-});
-
-listaTarefas.forEach((tarefa) => {
-  const novoElementoTarefa = criarElementoTarefa(tarefa);
-  elementoListaTarefas.innerHTML += novoElementoTarefa;
 });
